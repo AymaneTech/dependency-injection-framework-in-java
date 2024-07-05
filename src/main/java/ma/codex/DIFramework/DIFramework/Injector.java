25a6d6a5aa0f8ff4893672fe6d8a35984d004580
geek <elmainiaymane03@gmail.com>
2024-07-05 19:37:03 +0100

    clean the code and add the autowiring funcionnality

--- src/main/java/ma/codex/DIFramework/DIFramework/Injector.java
+++ src/main/java/ma/codex/DIFramework/DIFramework/Injector.java
@@ -7,65 +7,84 @@ import ma.codex.DIFramework.Utils.Scanners.ScanByAnnotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
+import java.lang.reflect.Parameter;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Injector {
-
-    /*
-     * Map to hold all components annotated with @Component
-     * */
     private HashMap<String, Object> components = new HashMap<>();
 
-    /*
-     * scanClasses: Scans all classes in the specified package, checks for dependencies,
-     * creates instances, and stores them in the components map.
-     */
     public void scanClasses() {
         try {
             ScanByAnnotation scanner = new ScanByAnnotation(Component.class);
             Collection<Class<?>> result = scanner.find("ma/codex/DIFramework");
 
-            result.stream().parallel().forEach((cls -> {
-                Constructor<?>[] constructors = cls.getConstructors();
-                Field[] fields = cls.getDeclaredFields();
-                AtomicBoolean hasDependencies = new AtomicBoolean(false);
-
-                Arrays.stream(constructors).parallel().forEach((constructor) -> {
-                    if (constructor.isAnnotationPresent(Autowired.class)) {
-                        inject(constructor.getDeclaringClass());
-                        hasDependencies.set(true);
-                    }
-                });
-                Arrays.stream(fields).parallel().forEach((field) -> {
-                    if (field.isAnnotationPresent(Autowired.class)) {
-                        inject(field.getDeclaringClass());
-                        hasDependencies.set(true);
-                    }
-                });
-                if (!hasDependencies.get()) {
-                    try {
-                        Object instance = constructors[0].newInstance();
-                        components.put(cls.getSimpleName(), instance);
-                    } catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {
-                        System.err.println("Error creating instance for class: " + cls.getName());
-                        e.printStackTrace();
-                    }
-                }
-            }));
+            result.forEach(this::instantiate);
             System.out.println(components.toString());
         } catch (RuntimeException e) {
             System.err.println("Error during class scanning and component creation.");
-            e.printStackTrace();        }
+            e.printStackTrace();
+        }
     }
 
-    /*
-     * inject: Injects dependencies into the specified class.
-     */
     private void inject(Class<?> declaringClass) {
+        Constructor<?>[] constructors = declaringClass.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            if (constructor.isAnnotationPresent(Autowired.class)) {
+                Parameter[] params = constructor.getParameters();
+                Object[] paramInstances = new Object[params.length];
 
+                for (int i = 0; i < params.length; i++) {
+                    Class<?> paramType = params[i].getType();
+                    paramInstances[i] = getOrCreateInstance(paramType);
+                }
+
+                try {
+                    Object instance = constructor.newInstance(paramInstances);
+                    components.put(declaringClass.getName(), instance);
+                    injectFields(instance);
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException("Error creating instance for " + declaringClass.getName(), e);
+                }
+                return;
+            }
+        }
+
+        // If no @Autowired constructor, use the default constructor
+        try {
+            Object instance = declaringClass.getDeclaredConstructor().newInstance();
+            components.put(declaringClass.getName(), instance);
+            injectFields(instance);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException("Error creating instance for " + declaringClass.getName(), e);
+        }
     }
 
+    private void instantiate(Class<?> clazz) {
+        if (!components.containsKey(clazz.getName())) {
+            inject(clazz);
+        }
+    }
+
+    private Object getOrCreateInstance(Class<?> clazz) {
+        if (!components.containsKey(clazz.getName())) {
+            instantiate(clazz);
+        }
+        return components.get(clazz.getName());
+    }
+
+    private void injectFields(Object instance) {
+        Field[] fields = instance.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+                field.setAccessible(true);
+                try {
+                    field.set(instance, getOrCreateInstance(field.getType()));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Error injecting field " + field.getName(), e);
+                }
+            }
+        }
+    }
 }
