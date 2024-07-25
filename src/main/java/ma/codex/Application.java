32d79d6b405ade9d4adaaa07eeac3e0f791b267f
geek <elmainiaymane03@gmail.com>
2024-07-25 14:33:56 +0100

    add a plugin to define the main class

--- src/main/java/ma/codex/Application.java
+++ src/main/java/ma/codex/Application.java
@@ -1,18 +1,17 @@
 package ma.codex;
 
-import ma.codex.Framework.Kernel;
-import ma.codex.Framework.ORM.JPRepository.JPRepository;
-import ma.codex.Framework.ORM.JPRepository.JPRepositoryImpl;
-import ma.codex.Framework.ORM.ShcemaManager.Core.QueryExecutor;
-import ma.codex.TestingCode.Entities.Category;
-
 import java.sql.SQLException;
+
+import ma.codex.Framework.Kernel;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
         Kernel.run(Application.class);
-        JPRepository repository = new JPRepositoryImpl<Class<Category>, Long>(new QueryExecutor());
-
-
+        System.out.println("here is the world");
+        
     }
 }
+
+
+
+
