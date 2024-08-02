03ef0dc41b4931a7f2570001fba5e9fefbc2b31b
geek <elmainiaymane03@gmail.com>
2024-08-02 12:56:05 +0100

    remove jpa repository part until later

--- src/main/java/ma/codex/Framework/ORM/JPRepository/JPRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.codex.Framework.ORM.JPRepository;
-
-public interface JPRepository <Entity, ID> extends ListRepository<Entity, ID>{
-    Entity save(Entity entity);
-
-    Entity findById(ID id);
-
-    boolean deleteById(ID id);
-}
